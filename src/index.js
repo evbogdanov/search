b45127494725298060b819b06c5d8a2bf8474a8a
Ev Bogdanov <evbogdanov.com@gmail.com>
2024-06-18 18:29:07 +0300

    Drop SW

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
 import { searches } from './searches/';
 
 ReactDOM.render(<App searches={searches} />, document.getElementById('app'));
